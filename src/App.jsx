c8cbccccafc231b7566c8bb1b299381101a8210f
John Pathadan <john.paul@vonnue.com>
2023-12-20 11:23:36 +0530

    feat: spacing beginning

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
 function App() {
   return (
-    <div
-      style={{
-        width: "400px",
-      }}
-    >
-      <h1 className="text-3xl italic ">Title 1</h1>
-      <h2 className="text-2xl line-through">Title 2</h2>
-      <h3 className="text-xl overline">Title 3</h3>
-      <p className="text-base leading-loose underline decoration-red-500 decoration-wavy decoration-1 underline-offset-4 capitalize">
-        a regular paragraph
-      </p>
-      <p className="text-sm lowercase">A description paragraph</p>
-      <p className="note text-xs uppercase whitespace-nowrap">A little note</p>
-      <p className="truncate">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quo
-        voluptates fuga architecto, fugit numquam? Quam recusandae voluptatibus
-        laudantium pariatur nobis corporis nam ipsam facere aliquid doloribus,
-        omnis dolore sequi voluptas! Eos rem non error a, dicta neque iure
-        maiores nostrum reiciendis deserunt ut illum suscipit optio veritatis
-        quis! Aperiam assumenda optio, tenetur dolores cum placeat. Dolorem nemo
-        ut omnis.
-      </p>
+    <div>
+      <div className="w-16 h-16 bg-red-500" />
+      <div className="w-16 h-16 bg-green-500" />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,33 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    spacing: {
-      1: "8px",
-      2: "12px",
-      3: "16px",
-      4: "24px",
-      5: "32px",
-      6: "48px",
-    },
-    extend: {
-      fontSize: {
-        xs: "12px",
-        sm: "15px",
-        base: "18px",
-      },
-      colors: {
-        vuejs: {
-          100: "#3af736",
-          932: "#434252",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
