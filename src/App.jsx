d99c6aaa31d6504849d05384d94ea0d2a5ecf9a8
John Pathadan <john.paul@vonnue.com>
2023-12-22 11:54:39 +0530

    feat: animation completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 function App() {
   return (
     <div className="h-screen p-4 Parent">
-      {/* shadow-md shadow-lg, shadow-none ... 
-      shadow-inner - shadow inside the box
-      to add opacity to color - shadow-cyan-500/50(50 is the opacity)
-      */}
       <div className="inline-block p-4 text-black bg-white border rounded-lg shadow-md shadow-cyan-500">
+        <img
+          src="https://images.unsplash.com/photo-1703028408833-ff4033ce37ab?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="w-64 h-32 mb-4 skew-y-6"
+        />
         <h1 className="text-2xl">Hello</h1>
         <p className="mb-4">This is my Div</p>
-        <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 text-white">
+        <button className="px-3 py-2 rounded-lg cursor-pointer bg-cyan-500 shadow-md shadow-cyan-500/50 text-white opacity-40">
           Say Hello
         </button>
       </div>
